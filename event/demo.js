2d24df46bcbe48f68b1d16a41608e9262b739569
leemove <limu@xshengya.com>
2017-10-11 16:05:49 +0800

    监听器注册时间

--- event/demo.js
+++ event/demo.js
@@ -4,8 +4,14 @@ class MyEvent extends EventEmitter {}
 
 const chifan = new MyEvent()
 
+chifan.once('newListener',function (event)  {
+  console.log(`注册了${event}事件的监听器`)
+  this.on(event, () => {console.log('在newListener中注册的事件被触发了')})
+})
 chifan.on('eat', function (a) {
-  console.log(a, '开饭了')
+  process.nextTick(() => {
+    console.log(a)
+  })
 })
 new Promise(function (resolve, reject) {
   setTimeout(function() {
